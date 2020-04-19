11e4366c7eb78dd2a96bf8a0d247526ca3f75c8e
ximin <h4nn11113@gmail.com>
2020-04-19 16:12:51 -0700

    Add Stetho interceptor

--- app/src/main/java/com/laioffer/tinnews/TinNewsApplication.java
+++ app/src/main/java/com/laioffer/tinnews/TinNewsApplication.java
@@ -4,11 +4,14 @@ import android.app.Application;
 
 import com.ashokvarma.gander.Gander;
 import com.ashokvarma.gander.imdb.GanderIMDB;
+import com.facebook.stetho.Stetho;
+import com.facebook.stetho.urlconnection.StethoURLConnectionManager;
 
 public class TinNewsApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
         Gander.setGanderStorage(GanderIMDB.getInstance());
+        Stetho.initializeWithDefaults(this);
     }
 }
