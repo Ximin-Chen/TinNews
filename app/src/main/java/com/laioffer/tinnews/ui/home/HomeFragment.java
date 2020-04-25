145c38560508c49bc66e699b5dc6b5543e64df8b
ximin <h4nn11113@gmail.com>
2020-04-25 09:31:10 -0700

    upload new icon

--- app/src/main/java/com/laioffer/tinnews/ui/home/HomeFragment.java
+++ app/src/main/java/com/laioffer/tinnews/ui/home/HomeFragment.java
@@ -13,6 +13,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.laioffer.tinnews.R;
+import com.laioffer.tinnews.databinding.FragmentHomeBinding;
 import com.laioffer.tinnews.repository.NewsRepository;
 import com.laioffer.tinnews.repository.NewsViewModelFactory;
 
@@ -21,6 +22,7 @@ import com.laioffer.tinnews.repository.NewsViewModelFactory;
  */
 public class HomeFragment extends Fragment {
     private HomeViewModel viewModel;
+    private FragmentHomeBinding binding;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -30,9 +32,10 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
-
+    
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
